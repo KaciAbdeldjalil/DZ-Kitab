860d1722d81f891e67ae4d38019d2f4731700e29
AbdedjalilKaci <a_kaci@estin.dz>
2025-12-06 00:26:30 +0000

    add footer and header and hero section

---
+++ dz-kitab-frontend/src/pages/Landing-Page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Hero } from '../components/hero'
+export const Landingpage = () => {
+    return (
+        <div className="">
+            <Hero></Hero>
+        </div>
+    )
+}
