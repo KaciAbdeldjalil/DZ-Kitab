50580204ffda2710d7b42f02a71823b345a8e216
AbdedjalilKaci <a_kaci@estin.dz>
2025-12-13 15:36:58 +0000

    add css changes

---
+++ dz-kitab-frontend/src/pages/AddNewAnnounce.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const AddNewAnnounce = () => {
+    return (
+        <div className="">
+            <h3>
+                
+            </h3>
+        </div>
+    )
+}
