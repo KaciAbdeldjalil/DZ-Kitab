f6947eac8a5e0c492241f7df9f17699cddcc1955
Yacine-Ms <y_messaoudi@estin.dz>
2026-01-05 03:29:10 +0100

    Add Message model for user communication

---
+++ dz-kitab-backend/app/models/message.py
@@ -0,0 +1,16 @@
+from django.db import models
+from .user import User
+from .announcement import Announcement
+
+class Message(models.Model):
+    sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
+    receiver = models.ForeignKey(User, related_name='received_messages', on_delete=models.CASCADE)
+    announcement = models.ForeignKey(Announcement, on_delete=models.CASCADE)
+
+    content = models.TextField()
+    is_read = models.BooleanField(default=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_at']
