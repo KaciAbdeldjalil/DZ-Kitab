e7e8bf1c365d9d94051df01b2626413cff74a624
Yacine-Ms <y_messaoudi@estin.dz>
2026-01-06 14:33:37 +0100

    Create Annonce model for book listings

---
+++ dz-kitab-backend/app/models/annonce.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Annonce(models.Model):
+    isbn = models.CharField(max_length=13, unique=True)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='annonces/', null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.title} ({self.isbn})"
