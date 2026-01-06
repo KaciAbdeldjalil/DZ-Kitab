759d3f528ef6bbfed79117dd5fbbdecb47fc82ae
Yacine-Ms <y_messaoudi@estin.dz>
2026-01-06 14:51:44 +0100

    Update urlpatterns to integrate DRF router
    Refactor urlpatterns to include DRF router URLs and remove duplicate imports.

--- dz-kitab-backend/app/urls.py
+++ dz-kitab-backend/app/urls.py
@@ -1,27 +1,25 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from app.views.search_views import AnnouncementAdvancedSearchView
 from app.views.message_views import SendMessageView, ConversationListView, AnnouncementMessagesView
 from app.views.favorite_views import FavoriteView, FavoriteDeleteView
-from rest_framework.routers import DefaultRouter
 from app.views.annonce_views import AnnonceViewSet
 
+# Routes classiques
 urlpatterns = [
     path('announcements/search/', AnnouncementAdvancedSearchView.as_view(), name='announcement-advanced-search'),
-]
-from .views.message_views import SendMessageView, ConversationListView, AnnouncementMessagesView
 
-urlpatterns += [
     path('messages/', SendMessageView.as_view(), name='send-message'),
     path('messages/conversations/', ConversationListView.as_view(), name='conversation-list'),
     path('messages/<int:announcement_id>/', AnnouncementMessagesView.as_view(), name='announcement-messages'),
-]
-urlpatterns += [
+
     path('favorites/', FavoriteView.as_view()),
     path('favorites/<int:id>/', FavoriteDeleteView.as_view()),
 ]
 
+# Routes CRUD DRF
 router = DefaultRouter()
 router.register(r'annonces', AnnonceViewSet, basename='annonce')
 
-urlpatterns = router.urls
-
+# Ajouter les routes DRF au urlpatterns existants
+urlpatterns += router.urls
