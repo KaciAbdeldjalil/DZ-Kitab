caf37d0df77ebabb6b074324a6672ceece99fdc7
Yacine-Ms <y_messaoudi@estin.dz>
2026-01-06 14:37:01 +0100

    Add router for AnnonceViewSet in URLs

--- dz-kitab-backend/app/urls.py
+++ dz-kitab-backend/app/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from app.views.search_views import AnnouncementAdvancedSearchView
 from app.views.message_views import SendMessageView, ConversationListView, AnnouncementMessagesView
 from app.views.favorite_views import FavoriteView, FavoriteDeleteView
+from rest_framework.routers import DefaultRouter
+from app.views.annonce_views import AnnonceViewSet
 
 urlpatterns = [
     path('announcements/search/', AnnouncementAdvancedSearchView.as_view(), name='announcement-advanced-search'),
@@ -17,4 +19,9 @@ urlpatterns += [
     path('favorites/', FavoriteView.as_view()),
     path('favorites/<int:id>/', FavoriteDeleteView.as_view()),
 ]
+
+router = DefaultRouter()
+router.register(r'annonces', AnnonceViewSet, basename='annonce')
+
+urlpatterns = router.urls
 
