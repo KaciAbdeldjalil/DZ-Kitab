e5c4f48ca81986f809d597def711762891ac543b
Yacine-Ms <y_messaoudi@estin.dz>
2026-01-14 08:37:23 +0100

    Modify Annonce model fields and types
    Updated fields in the Annonce model to include category, market price, final calculated price, page count, and publication date. Changed created_at to not auto-add and modified image field type.

--- dz-kitab-backend/app/models/annonce.py
+++ dz-kitab-backend/app/models/annonce.py
@@ -5,8 +5,15 @@ class Annonce(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-    image = models.ImageField(upload_to='annonces/', null=True, blank=True)
+    created_at = models.DateTimeField()
+    image = models.CharField(max_length=255, null=True)
 
-    def __str__(self):
-        return f"{self.title} ({self.isbn})"
+    category = models.CharField(max_length=50)
+    market_price = models.FloatField(null=True)
+    final_calculated_price = models.FloatField(null=True)
+    page_count = models.IntegerField(null=True)
+    publication_date = models.CharField(max_length=50, null=True)
+
+    class Meta:
+        managed = False   # ⚠️ CRUCIAL : on ne laisse pas Django toucher la DB
+        db_table = 'announcements'  # on lie au vrai nom SQL
