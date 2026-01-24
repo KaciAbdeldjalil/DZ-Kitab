7e1108ad643baf05d7617520964ab2f06c679cfb
Manadi.T <150386821+tasmd@users.noreply.github.com>
2026-01-24 21:29:30 +0100

    Delete dz-kitab-backend/app/serializers directory

--- dz-kitab-backend/app/serializers/annonce_serializer.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from app.models.book import Announcement  # ou Annonce si renommé
-
-class AnnouncementSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Announcement
-        fields = '__all__'  # ou liste explicite : ['id', 'book', 'user', 'price', ...]
--- dz-kitab-backend/app/serializers/message_serializer.py
+++
@@ -1,8 +0,0 @@
-# app/serializers/message_serializer.py
-from rest_framework import serializers
-from app.models.message import Message
-
-class MessageSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Message
-        fields = ['IdM', 'sender', 'receiver', 'annonce', 'contenu', 'etat', 'reponses']
