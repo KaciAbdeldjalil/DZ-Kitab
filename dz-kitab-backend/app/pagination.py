84a26a59dc7e24b527b655fb80474409014d09bb
Manadi.T <150386821+tasmd@users.noreply.github.com>
2026-01-24 21:32:10 +0100

    Update pagination.py

--- dz-kitab-backend/app/pagination.py
+++ dz-kitab-backend/app/pagination.py
@@ -1,6 +1,1 @@
-from rest_framework.pagination import PageNumberPagination
 
-class AnnouncementSearchPagination(PageNumberPagination):
-    page_size = 20
-    page_size_query_param = 'page_size'
-    max_page_size = 100
