c78d935de54baa7c06ff6b0eb4c2787e6726fb17
Manadi.T <150386821+tasmd@users.noreply.github.com>
2026-01-24 21:29:04 +0100

    Delete dz-kitab-backend/app/views directory

--- dz-kitab-backend/app/views/message_views.py
+++
@@ -1,8 +0,0 @@
-# app/views/message_views.py
-from rest_framework import viewsets
-from app.models.message import Message
-from app.serializers.message_serializer import MessageSerializer
-
-class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
-    serializer_class = MessageSerializer
